948683a609db1f69ba9e869e3cfbfc917b91e915
Oleg Shibaev <oshibaev@oggettoweb.com>
2019-03-31 02:59:00 +0300

    Initial commit

---
+++ plantMonitor.h
@@ -0,0 +1,54 @@
+struct Param {
+    const String name;
+    const String shortName;
+    const int minValue;
+    const int maxValue;
+    const String messageExceeding;
+    const String messageLacking;
+    const bool invertMinMax;
+    const int decimalPlaces;
+
+    float (*readValue)();
+
+    float cachedValue;
+};
+
+/* SETUP */
+void setup();
+
+/* INIT */
+void initOutput();
+void initRandom();
+void initScreen();
+
+/* LOOP */
+void loop();
+
+/* LIGHT */
+void turnLightOn();
+void turnLightOff();
+
+/* SOUND */
+void playAlert();
+
+/* CONSOLE */
+void writeAllValues();
+void writeValue(String str, float value, int decimalPlaces);
+
+/* GET VALUES */
+void updateAllValues();
+float getSoilHumidity();
+float getAmbientBrightness();
+float getAirHumidity();
+float getAirTemperature();
+
+/* CHECK VALUES */
+bool isAllParamsOk();
+bool isParamOk(Param *param);
+
+/* SCREEN */
+void resetScreen();
+void displayAllValues();
+void displayWarning(String warning);
+void displayLineEnd();
+void displayValue(String abr, float value, int decimalPlaces);
---
+++ plantMonitorConfig.h
@@ -0,0 +1,29 @@
+// DHT
+#define DHT_TYPE DHT22
+
+// INPUT PINS
+#define PIN_A_PHOTOSENSOR 1
+#define PIN_A_SOILHUMIDITY 0
+#define PIN_D_DHT 7
+
+// OUTPUT PINS
+#define PIN_D_BUZZER 6
+#define PIN_D_LIGHT 13
+
+// LCD DISPLAY PINS
+#define PIN_LCD_1 12
+#define PIN_LCD_2 11
+#define PIN_LCD_3 5
+#define PIN_LCD_4 4
+#define PIN_LCD_5 3
+#define PIN_LCD_6 2
+
+// SYSTEM CONFIG
+#define PORT 9600
+#define INIT_DELAY 2000
+#define UPDATE_INTERVAL 1000
+
+// DISPLAY
+#define DISPLAY_WIDTH 16
+#define DISPLAY_HEIGHT 2
+#define DISPLAY_GREETING "HELLO!"
